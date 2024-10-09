300df7b2de781210d654bc728b79e775595731e6
Levi-Lesches <levilesches@gmail.com>
2024-10-09 08:12:20 +0000

    Deploying to gh-pages from @ Levi-Lesches/flutter_resizable_container@a4a3d749447afd812ada0c3def4ef5c886a43c21 🚀

---
+++ assets/lib/screens/divider/custom_divider_example_screen.dart
@@ -0,0 +1,172 @@
+import 'package:example/file_asset_paths.dart';
+import 'package:example/widgets/code_view_dialog.dart';
+import 'package:example/widgets/nav_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_resizable_container/flutter_resizable_container.dart';
+
+class CustomDividerExampleScreen extends StatefulWidget {
+  const CustomDividerExampleScreen({super.key});
+
+  @override
+  State<CustomDividerExampleScreen> createState() =>
+      _CustomDividerExampleScreenState();
+}
+
+class _CustomDividerExampleScreenState
+    extends State<CustomDividerExampleScreen> {
+  var hovered = false;
+  var length = 0.5;
+  var thickness = 2.0;
+  var padding = 5.0;
+  var crossAxisAlignment = CrossAxisAlignment.center;
+  var mainAxisAlignment = MainAxisAlignment.center;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom divider example'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () => CodeViewDialog.show(
+              context: context,
+              filePath: FileAssetPaths.dividerScreen,
+            ),
+            icon: const Icon(Icons.code),
+          ),
+        ],
+      ),
+      drawer: const NavDrawer(),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const Text('Length'),
+                  Slider(
+                    min: 0.01,
+                    max: 1.0,
+                    value: length,
+                    onChanged: (value) => setState(() => length = value),
+                  ),
+                  Text('Ratio: ${(length * 100).toStringAsFixed((2))}%'),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text('Thickness'),
+                  Slider(
+                    min: 1,
+                    max: 20.0,
+                    divisions: 19,
+                    value: thickness,
+                    onChanged: (value) => setState(() => thickness = value),
+                  ),
+                  Text('${thickness}px'),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text('Padding'),
+                  Slider(
+                    min: 0,
+                    max: 20,
+                    divisions: 20,
+                    value: padding,
+                    onChanged: (value) => setState(() => padding = value),
+                  ),
+                  Text('${padding}px'),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text('Cross-Axis Alignment'),
+                  DropdownButton(
+                    value: crossAxisAlignment,
+                    items: const [
+                      DropdownMenuItem(
+                        value: CrossAxisAlignment.start,
+                        child: Text('Start'),
+                      ),
+                      DropdownMenuItem(
+                        value: CrossAxisAlignment.center,
+                        child: Text('Center'),
+                      ),
+                      DropdownMenuItem(
+                        value: CrossAxisAlignment.end,
+                        child: Text('End'),
+                      ),
+                    ],
+                    onChanged: (value) => setState(
+                      () => crossAxisAlignment = value!,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text('Main-Axis Alignment'),
+                  DropdownButton(
+                    value: mainAxisAlignment,
+                    items: const [
+                      DropdownMenuItem(
+                        value: MainAxisAlignment.start,
+                        child: Text('Start'),
+                      ),
+                      DropdownMenuItem(
+                        value: MainAxisAlignment.center,
+                        child: Text('Center'),
+                      ),
+                      DropdownMenuItem(
+                        value: MainAxisAlignment.end,
+                        child: Text('End'),
+                      ),
+                    ],
+                    onChanged: (value) => setState(
+                      () => mainAxisAlignment = value!,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ResizableContainer(
+              direction: Axis.horizontal,
+              divider: ResizableDivider(
+                color: hovered
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.inversePrimary,
+                thickness: thickness,
+                padding: padding,
+                crossAxisAlignment: crossAxisAlignment,
+                mainAxisAlignment: mainAxisAlignment,
+                length: ResizableSize.ratio(length),
+                onHoverEnter: () => setState(() => hovered = true),
+                onHoverExit: () => setState(() => hovered = false),
+              ),
+              children: [
+                ResizableChild(
+                  child: ColoredBox(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    child: const Center(child: Text('Left')),
+                  ),
+                ),
+                ResizableChild(
+                  child: ColoredBox(
+                    color: Theme.of(context).colorScheme.tertiaryContainer,
+                    child: const Center(child: Text('Right')),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_service_worker.js
@@ -0,0 +1,212 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
+"main.dart.js": "a38c573ac517d040990b553994d2966e",
+"assets/FontManifest.json": "f8c5386378f30b925622abfa4765e0da",
+"assets/AssetManifest.bin": "1d14d42af5021b4138e3d5d77710ffaf",
+"assets/fonts/MaterialIcons-Regular.otf": "10cffa28c74e6282bfb7b80d2ea5b664",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/assets/fonts/RedditMono-VariableFont_wght.ttf": "3678f8f5122391aa145683c9d7b6af9f",
+"assets/lib/screens/divider/custom_divider_example_screen.dart": "aa0e34dc2f33db499a538ed2464d09e4",
+"assets/lib/screens/controller_set_sizes/controller_set_sizes_example_screen.dart": "ea800e8c301ea82a055670cb54c56a0c",
+"assets/lib/screens/basic/basic_example_screen.dart": "28c5fda7ca0e46dd7273facadebea7bc",
+"assets/lib/screens/controller_listen/controller_listen_example_screen.dart": "c3c9c74a70bed06feb304224edd63f90",
+"assets/lib/screens/ratio/ratio_example_screen.dart": "a52831ac4d47d2389bd0b2f8d152878a",
+"assets/lib/screens/pixels/pixels_example_screen.dart": "64b4fe895c29a96da14f249fd6d687d6",
+"assets/NOTICES": "c2d08f328340030849d869ae4713ab15",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.json": "52c3b3cd79dd9e2a8162125a013a1f22",
+"assets/AssetManifest.bin.json": "84a2e7e905005a39e55353cbbf8a18e4",
+"index.html": "f27c2489e9651b03063b05fb3f90f6fa",
+"/": "f27c2489e9651b03063b05fb3f90f6fa",
+"manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08",
+"canvaskit/canvaskit.js": "738255d00768497e86aa4ca510cce1e1",
+"canvaskit/canvaskit.js.symbols": "74a84c23f5ada42fe063514c587968c6",
+"canvaskit/chromium/canvaskit.js": "901bb9e28fac643b7da75ecfd3339f3f",
+"canvaskit/chromium/canvaskit.js.symbols": "ee7e331f7f5bbf5ec937737542112372",
+"canvaskit/chromium/canvaskit.wasm": "399e2344480862e2dfa26f12fa5891d7",
+"canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
+"canvaskit/skwasm.js.symbols": "c3c05bd50bdf59da8626bbe446ce65a3",
+"canvaskit/canvaskit.wasm": "9251bb81ae8464c4df3b072f84aa969b",
+"canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
+"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "d1a3ec4585d541eb5b82d9faaa36760c",
+"flutter_bootstrap.js": "570dc933c5b10032fde7f86e1636c3e8"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
